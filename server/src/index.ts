f70e2efa9629f9a19b960e03e6231096c0c16631
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2025-11-22 18:06:03 +0800

    feat: add rate limitting

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,25 @@ import cors from "cors";
 import http from "http";
 import userRoutes from "./routes/user";
 import productRoutes from "./routes/product";
+import rateLimit from "express-rate-limit";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 60 * 1000, 
+  max: 300,                 
+  message: {
+    status: 429,
+    message: "Too many requests, please try again later.",
+  },
+});
+
 
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
