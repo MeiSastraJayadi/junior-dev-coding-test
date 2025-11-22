1ae41d6e12a6ae359577585610db68a8cb3d90b3
mei4ecap <mei@kronosinvestments.net>
2025-11-22 08:50:49 +0800

    fix: api base url imported in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from "dotenv";
 
+dotenv.config();
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
